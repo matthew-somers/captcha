a417f5b18f1fbb56d29e395725b9c6476194d9e9
Matthew Somers <matthew.somers.hi@gmail.com>
2013-10-13 22:37:26 -0700

    added second case

--- src/captcha.java
+++ src/captcha.java
@@ -15,22 +15,30 @@ public class captcha
 	public static void main(String[] args) 
 	{
 		String[] wordlist = {"holycrap", "nopleasestop", "ohgodwhy",
-				"dungeon", "sosleepy", "whatishappening"};
+				"dungeon", "sosleepy", "whatishappening", "lostandalone"};
 		
-		System.out.println("Solution is every 3rd character of the CAPTCHA!");
+		Random ran = new Random();	
 		
-		Random ran = new Random();
+		int chooser = ran.nextInt(2);
+		if (chooser == 0)
+			System.out.println("Solution is every 3rd character of the CAPTCHA!");
+		else
+			System.out.println("Solution is every 2nd character of the CAPTCHA!");
+		
+
 		int rand = ran.nextInt(wordlist.length);
 		char rand2;
 		char rand3;
 		System.out.print("CAPTCHA: ");
 		for (int i = 0; i < wordlist[rand].length(); i++)
 		{
-			rand2 = (char) (ASCIISTART + ran.nextInt(ASCIILENGTH));
-			rand3 = (char) (ASCIISTART + ran.nextInt(ASCIILENGTH));
+			rand2 = (char) (ASCIISTART + ran.nextInt(ASCIILENGTH+1));
+			rand3 = (char) (ASCIISTART + ran.nextInt(ASCIILENGTH+1));
 
 			System.out.print(rand2);
-			System.out.print(rand3);
+			
+			if (chooser == 0)
+				System.out.print(rand3);
 			System.out.print(wordlist[rand].charAt(i));
 		}
 		
